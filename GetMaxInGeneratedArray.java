5db32368d0eb7829eedf0ca12c83c896bdae7c2b
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2021-01-15 23:34:20 +0530

    Add files via upload

---
+++ GetMaxInGeneratedArray.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int getMaximumGenerated(int n) {
+        if(n<=1)
+            return n;
+        int arr[]=new int[n+1];
+        arr[0]=0;
+        arr[1]=1;
+        int max=0;
+        max=Math.max(arr[0],arr[1]);
+        for(int i=2;i<=n;i++)
+        {
+            if(i%2==0)
+             arr[i]=arr[i/2];
+             
+            else
+                arr[i]=arr[i/2]+arr[i/2+1];
+            max=Math.max(arr[i],max);
+        }
+        return max;
+    }
+}
