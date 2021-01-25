c02c5432b8627cd70d25bdc6a6b44267e095d933
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2021-01-25 16:46:49 +0530

    Add files via upload

--- MergeKsortedList.java
+++ MergeKsortedList.java
@@ -64,3 +64,73 @@ class Solution {
         
     }
 }
+//////////////////////////////////////////////////////////////////////////////////
+//using mergeSort
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length==0)
+            return null;
+        return merge_sort(0,lists.length-1,lists);
+        
+        
+        
+    }
+    public ListNode merge_sort(int start,int end,ListNode[] lists)
+    { if(start==end)
+    {
+        return lists[start];
+    }
+        int mid=(start+end)/2;
+       ListNode left= merge_sort(start,mid,lists);
+       ListNode right= merge_sort(mid+1,end,lists);
+        return merge(left,right);
+    }
+    public ListNode merge(ListNode l1,ListNode l2)
+    {
+        ListNode dummy=new ListNode(0);
+        ListNode ans=dummy;
+        while(l1!=null || l2!=null)
+        {
+           while(l1!=null && l2!=null)
+           {
+               if(l1.val<l2.val)
+               {
+                   dummy.next=new ListNode(l1.val);
+                   dummy=dummy.next;
+                   l1=l1.next;
+               }
+               else
+               {
+                   dummy.next=new ListNode(l2.val);
+                   dummy=dummy.next;
+                   l2=l2.next;
+               }
+           }
+            if(l1!=null)
+            {
+                dummy.next=l1;
+                l1=null;
+                //l1=l1.next;
+                
+            }
+            if(l2!=null)
+                dummy.next=l2;
+            l2=null;
+            
+        }
+        
+        return ans.next;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
